fb0475c0008be82f9a56728388661c2109a48cc3
Paiman Rasoli <paimanrasoli789@gmail.com>
2023-04-03 00:20:26 +0430

    test and write the data to disk

--- request/get-data.go
+++ request/get-data.go
@@ -2,29 +2,50 @@ package request
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"math"
 	"net/http"
+	"os"
 	"time"
 )
 
 
-func printDetails(response *http.Response) {
-	var fileSizeInMB float64 = (float64(response.ContentLength) / 1024) / 1024
-	var fileType string = getType(response.Header.Get("Content-Type"))
+func printDetails(URL string) {
+	httpClient := &http.Client{Timeout: 15 * time.Second}
+	resp, err := httpClient.Head(URL)
+	if err != nil {
+		log.Fatalf("error on HEAD request: %s", err.Error())
+	    }
+	var fileSizeInMB float64 = math.Abs((float64(resp.ContentLength) / 1024) / 1024)
+	var fileType string = getType(resp.Header.Get("Content-Type"))
 	fmt.Printf("\n { FileSize : %.2f MB  , fileType : %s }\n", fileSizeInMB,fileType)
 }
 
 
 func FetchData(URL string){
-	httpClient := &http.Client{Timeout: 15 * time.Second}
-	resp, err := httpClient.Head(URL)
-
-	if err != nil {
-	  log.Fatalf("error on HEAD request: %s", err.Error())
-	}
-	printDetails(resp)
+	printDetails(URL)
 	mySpinner := lunchSpinner()
 	mySpinner.Start()
-	var data []byte
-	resp.Body.Read(data)
+	resp, err := http.Get(URL)
+	if err != nil{
+		log.Fatal(err)
+	}
+      defer resp.Body.Close()
+      out, err := os.Create("filename.png")
+      if err != nil {
+        log.Fatal(err)
+      }   
+     defer out.Close()
+     io.Copy(out, resp.Body)
+// 	mySpinner := lunchSpinner()
+// 	mySpinner.Start()
+// 	body, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	resp.Body.Close()
+// 	ioutil.WriteFile("adsda" ,body , 0777)
+// 	fmt.Println("Done 😀😀😀")
+// }
 }
--- request/spinner.go
+++ request/spinner.go
@@ -3,13 +3,11 @@ package request
 import (
 	"time"
 
-	"fmt"
-
 	"github.com/briandowns/spinner"
 )
 
 func lunchSpinner() *spinner.Spinner {
-	fmt.Print("Downloading ..... ")
-	s := spinner.New(spinner.CharSets[36], 100*time.Millisecond)  // Build our new spinner
+	s := spinner.New(spinner.CharSets[36], 100*time.Millisecond)
+      s.Prefix = "Downloading   "
 	return s
 }
